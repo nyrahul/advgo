dbf4d21174412c7042fa326d53e98b4112daf295
Rahul Jadhav <r@accuknox.com>
2021-12-26 23:05:12 +0530

    first commit

---
+++ factory/shoe/adidas/adidas.go
@@ -0,0 +1,22 @@
+package adidas
+
+type adidas struct {
+}
+
+func (a *adidas) makeShoe() iShoe {
+	return &adidasShoe{
+		shoe: shoe{
+			logo: "adidas",
+			size: 14,
+		},
+	}
+}
+
+func (a *adidas) makeShort() iShort {
+	return &adidasShort{
+		short: short{
+			logo: "adidas",
+			size: 14,
+		},
+	}
+}
